97f8bf633ad664eb805f0faa7be3db2eeaa55aad
Lucas Gabriel <37782940+v0ltmx@users.noreply.github.com>
2020-11-09 21:51:33 -0300

    Add files via upload

---
+++ scripts.R
@@ -0,0 +1,91 @@
+library(tidyverse)
+            #total/capital/interior    #NORTE
+universidadenortefederal <- (10,8,2)
+universidadenorteestadual <-(5,5,2)
+universidadenortemunicipal <-(0,0,0)
+
+centrosnortefederal <-(0,0,0)
+centrosnorteestadual <-(0,0,0)
+centrosnortemunicipal <-(1,0,1)
+
+faculdadefederal=(0,0,0)
+faculdadeestadual=(0,0,0)
+faculdademunicipal=(1,0,1)
+
+iffederal=(7,7,0)
+ifestadual=(0,0,0)
+ifmunicipal=(0,0,0)
+
+
+        #total/capital/interior #NORDESTE
+universidadenordestefederal <-(18,9,9)
+universidadenordesteestadual <-(15,6,9)
+universidadenordestemunicipal <-(2,2,0)
+
+centrosnordestefederal=(0,0,0)
+centrosnordesteestadual=(0,0,0)
+centrosnordestemunicipal=(0,0,0)
+
+faculdadefederal=(0,0,0)
+faculdadeestadual=(1,1,0)
+faculdademunicipal=(22,0,22)
+
+iffederal=(11,10,1)
+ifestadual=(0,0,0)
+ifmunicipal=(0,0,0)
+
+
+        #total/capital/interior #CENTROESTE
+universidadecentrofederal=(5,4,1)
+universidadecentroestadual=(3,0,3)
+universidadecentromunicipal=(1,0,4)
+
+centrocentrofederal=(0,0,5)
+centrocentroestadual=(0,0,0)
+centrocentromunicipal=(1,0,1)
+
+faculdadefederal=(1,1,0)
+faculdadeestadual=(1,1,0)
+faculdademunicipal=(2,0,2)
+
+iffederal=(5,5,0)
+ifestadual=(0,0,0)
+ifmunicipal=(0,0,0)
+
+
+        #total/capital/interior #SUL
+universidadesulfederal=(11,5,6)
+universidadesulestadual=(9,2,7)
+universidadesulmunicipal=(1,0,1)
+
+centrosulfederal=(0,0,0)
+centrosulestadual=(0,0,0)
+centrosulmunicipal=(2,0,2)
+
+faculdadesulfederal==(0,0,0)
+faculdadesulestadual(=0,0,0)
+faculdadesulmunicipal=(3,0,3)
+
+iffederal=(6,2,4)
+ifestadual=(0,0,0)
+ifmunicipal=(0,0,0)
+
+
+        #total/capital/interior #SUDESTE
+universidadesudestefederal=(19,5,14)
+universidadesudesteestadual=(8,5,3)
+universidadesudestemunicipal=(2,0,2)
+
+centrosudestefederal=(2,1,1)
+centrosudesteestadual=(2,1,1)
+centrosudestemunicipal=(5,0,5)
+
+faculdadesudestefederal=(4,3,1)
+faculdadesudesteestadual=(84,11,73)
+faculdadesudestemunicipal=(20,0,20)
+
+iffederal=(11,6,5)
+ifestadual=(0,0,0)
+ifmunicipal=(0,0,0)
+
+##ENB
